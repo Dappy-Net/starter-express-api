3052624d0b2c9bec100302cf7e093c221d6500e9
DappyNetwork <92771259+Dappy-Net@users.noreply.github.com>
2023-11-26 19:11:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(express.urlencoded({ extended: true }));
+
 
 app.all('/', (req, res) => {
-    res.send('hello!')
+   res.render('index', { title: 'My Express App' });
 })
 
 app.use('/static',express.static('./static'))
